a3ca782a01c43bd7cff9ac9c389fba02245ce578
MosesECSS03 <moses_lee@ecss.org.sg>
2025-02-28 13:47:06 +0800

    Update Slowly v2

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,16 @@ var logger = require('morgan');
 var cors = require("cors");
 var app = express(); // Initialize the Express app
 
+var cors = require("cors");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require("./routes/login");
+var coursesRegistrationRouter = require("./routes/courseregistration");
+var accountDetailsRouter = require("./routes/accountDetails");
+var accessRightsRouter = require("./routes/accessRights");
+var receiptRouter = require("./routes/receipt");
+var invoiceRouter = require("./routes/invoice");
+
 // Use middleware
 app.use(cors()); // Enable CORS
 app.use(logger('dev')); // HTTP request logger
@@ -17,10 +27,26 @@ app.use(cookieParser()); // For parsing cookies
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); // You can change to 'ejs' or others if needed
 
-// Define a route for the root path
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(logger('dev'));
+app.use(cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add any other methods you want to support
+  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Disposition'], 
+  exposedHeaders: ['Content-Disposition'], // Add this line to expose the header
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/login", loginRouter);
+app.use("/courseregistration", coursesRegistrationRouter);
+app.use("/accountDetails", accountDetailsRouter);
+app.use("/accessRights", accessRightsRouter);
+app.use("/receipt", receiptRouter);
+app.use("/invoice", invoiceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
