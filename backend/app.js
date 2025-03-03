68d893b4d8f4b006a974f9d2ed5d87edd5f71a3f
MosesECSS03 <moses_lee@ecss.org.sg>
2025-03-03 17:21:01 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cors()); // Enable CORS
 app.use(logger('dev')); // HTTP request logger
 app.use(express.json()); // For parsing JSON
 app.use(express.urlencoded({ extended: false })); // For parsing URL-encoded data
-app.use(cookieParser()); // For parsing cookies
+app.use(cookieParser()); // For parsing cookies ok
 
 // Set up views (if you're using templates)
 app.set('views', path.join(__dirname, 'views'));
