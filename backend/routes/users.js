b082fd362a5ab0f5454c8a6d7d86e30a8cf7aa70
MosesECSS03 <175521933+MosesECSS03@users.noreply.github.com>
2025-05-27 15:12:30 +0800

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var router = express.Router();
-var ParticpantsController = require('../Controller/Participants/ParticipantsController');
+var ParticipantsController = require('../Controller/Participants/ParticipantsController');
 
 router.post("/", async function(req, res) 
 {
     var userName = req.body.userName;
     var password = req.body.password;
     //console.log(userName, password);
-    var controller = new ParticpantsController();
+    var controller = new ParticipantsController();
     var result = await controller.login(userName, password);
     console.log(result);
     res.json({"message": result});
